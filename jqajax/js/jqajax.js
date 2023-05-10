e7ea9c755b08a0cbce27b0351d5e1e5b195d3acb
Sylesh <chpsylesh@gmail.com>
2023-05-10 21:49:06 +0530

    adding more jquery code

--- jqajax/js/jqajax.js
+++ jqajax/js/jqajax.js
@@ -3,21 +3,58 @@ $(document).ready(function(){
         $.ajax({
         url: "https://jsonplaceholder.typicode.com/users",
         type: 'GET',
+        xhrFields: {
+            onprogress: function(){
+                str = "Loading....";
+            $('#data').html(str);
+            }
+        },
         success: function(users){
             str = `<table><tr>`;
-            for (key in users) {
-                str += `<th>${key}</th>`;
+            var firstUser = users[0];
+                for (var key in firstUser) {
+                    str += `<th>${key}</th>`;
                 }
-                str +=`</tr><tr>`;
-            for (user in users) {
-            str += `<td>${user.id}</td>`;
+                str +=`</tr>`;
+                for (var i = 0; i < users.length; i++) {
+                    var user = users[i];
+                str += `<tr>
+                    <td>${user.id}</td>
+                    <td>${user.name}</td>
+                    <td>${user.username}</td>
+                    <td>${user.email}</td>
+                    <td>${user.address.street}, ${user.address.city}, ${user.address.zipcode}</td>
+                    <td>${user.phone}</td>
+                    <td>${user.website}</td>
+                    <td>${user.company.name}, ${user.company.catchPhrase}, ${user.company.bs}</td>
+                    </tr>`;
             }
-        str+= `</tr></table>`;
+            // for (var key in users[0]) {
+            //     str += `<th>${key}</th>`;
+            // }
+            // str +=`</tr><tr>`;
+            // for (var i = 0; i < users.length; i++) {
+            //     var user = users[i];
+            //     for (var prop in user) {
+            //         if (prop === "address") {
+            //             var address = user[prop];
+            //             str += `<td>${address.street}, ${address.city}, ${address.zipcode}</td>`;
+            //         } else if (prop === "company") {
+            //             var company=user[prop];
+            //             str += `<td>${company.name}, ${company.catchPhrase}, ${company.bs}</td>`;
+            //         } else {
+            //             str += `<td>${user[prop]}</td>`;
+            //         }
+            //     }
+            //     str += `</tr><tr>`;
+            // }
+        str+= `</table>`;
             $('#data').html(str);
-            console.log(data);
+            console.log(users);
         },
-        error : function(data){
-            console.log(data);
+        error : function(){
+            str = "Error";
+            $('#data').html(str);
         } 
         });
     });
